904b53a0e02638afc9c807513bd9b5c0626e9b91
ajunake <kees.vincent.ajunan@oocl.com>
2021-08-19 21:04:39 +0800

    [Integration][Kevin]test: add test: should_return_all_todo_when_getAllTodos()

---
+++ src/test/java/todoIntegrationTest/TodoIntegrationTest.java
@@ -0,0 +1,30 @@
+package todoIntegrationTest;
+
+import com.example.todolist.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class TodoIntegrationTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private TodoRepository todoRepository;
+
+    @Test
+    void should_return_all_todo_when_getAllTodos() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/todos"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].text").value("first to do item"))
+                .andExpect(jsonPath("$[1].text").value("second to do item"));
+
+    }
+}
